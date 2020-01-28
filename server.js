bb776200b2916d436aee5cd64ef2963ddd6bd8e3
Rickson2019 <2528889836@qq.com>
2020-01-28 12:14:42 -0800

    template modified 01

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 const express = require('express');
-
+var MongoClient = require('mongodb').MongoClient;
 const app = express();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
 
+let visitorNum = 0;
 
-const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+    visitorNum++;
+    console.log("Visitor" + visitorNum);
+    res.send("hello");
+    //res.send(<landingPage>)
+})
+
+app.get('/login', (req, res) => {
+
+    console.log("Visitor" + visitorNum + "is on the login page");
+    res.send("login");
+    //res.send(<loginPage>)
+})
+
+
+
+console.log("welcome to Project Hub");
+const PORT = process.env.PORT || 90;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+
