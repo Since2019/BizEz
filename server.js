335a9df8bcb50f0ac49c63ee707a88c54311afa3
opinkfloyd <wenbojijack@gmail.com>
2020-02-01 13:22:06 -0800

    text_search

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 var MongoClient = require('mongodb').MongoClient;
 const app = express();
 const cors = require('cors');
+fs = require('fs');
+var formidable = require('formidable');
 
 var url =
   'mongodb+srv://helloworld:bcitteam28@cluster0-r8cwn.mongodb.net/test?retryWrites=true&w=majority';
@@ -14,7 +16,7 @@ let visitorNum = 0;
 const data = [];
 app.get('/', (req, res) => {
   console.log('1');
-  
+
   async function quickstart() {
     try {
       // Imports the Google Cloud client library
@@ -24,7 +26,7 @@ app.get('/', (req, res) => {
       const client = new vision.ImageAnnotatorClient();
 
       // Performs label detection on the image file
-      const [result] = await client.labelDetection(`./ipad.jpg`);
+      const [result] = await client.labelDetection(`./test.jpg`);
       const labels = result.labelAnnotations;
       // console.log('Labels:');
       labels.forEach(label => data.push(label.description));
@@ -37,8 +39,8 @@ app.get('/', (req, res) => {
           .findOne({ name: data[0] }, function(err, result) {
             if (err) throw err;
             db.close();
-            console.log(result);
-            
+            console.log('result' + result);
+
             res.json(result);
           });
       });
@@ -54,6 +56,54 @@ app.get('/login', (req, res) => {
   console.log('Visitor' + visitorNum + 'is on the login page');
   res.send('login');
   //res.send(<loginPage>)
+});
+
+// app.use(multer({dest:'/tamp/'}).array('isUpdata'))
+app.get('/upload', (req, res) => {
+  res.sendFile(__dirname + '/submit.html');
+});
+// app.post('/upData',(req,res)=>{
+//   console.log(req.files)
+//   var result = req.files;
+//   result.forEach((value)=>{
+//     var newName = value.originalname
+//     var readStream = fs.createReadStream(value.path);
+//     var writeStrea = fs.createWriteStream(newName);
+//     readStream.pipe(writeStrea);
+//     console.log(newName +'图片上传成功');
+//     readStream.on('error',()=>{console.log('上传失败')})
+//   })
+//   res.end();
+// })
+app.get('/query/:item', (req, res) => {
+  item = req.params.item + '';
+  item = item.substring(0, 1).toUpperCase() + item.substring(1, item.length);
+
+  console.log(item);
+
+  console.log('item' + item);
+  MongoClient.connect(url, async (err, db) => {
+    var bestbuy_store = await db.db('bestbuy_store');
+    if (err) throw err;
+    await bestbuy_store
+      .collection('products')
+      .findOne({ name: item }, function(err, result) {
+        if (err) throw err;
+        db.close();
+        console.log('result' + result);
+        res.json(result);
+      });
+  });
+});
+app.post('/upData', function(req, res) {
+  var form = new formidable.IncomingForm();
+  console.log('about to parse');
+  form.parse(req, function(error, fields, files) {
+    console.log('parsing done');
+    console.log(files.upload.path);
+    fs.writeFileSync('./test.jpg', fs.readFileSync(files.upload.path));
+    res.redirect('/');
+  });
 });
 
 console.log('welcome to Project Hub');
