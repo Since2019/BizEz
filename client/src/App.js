94aa395df1d03982f8e7a7bd883c28b80d9fcdd5
Yang Li <liyang0525@hotmail.com>
2020-02-01 01:40:32 -0800

    server.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+
+  const [hello,setHello] = useState(0);
+const [str,setStr] = useState('hi');
+  const onclick=()=>{
+    setHello(hello+1);
+    console.log(hello);
+    
+  }
+
   return (
     <div>
       <h1>biz hack</h1>
+      <button onClick={onclick}>Hello</button>
+      <h1>{hello}</h1>
     </div>
   );
 };
--- server.js
+++ server.js
@@ -8,10 +8,33 @@ app.use(express.json({ extended: false }));
 let visitorNum = 0;
 
 app.get('/', (req, res) => {
-    visitorNum++;
-    console.log("Visitor" + visitorNum);
-    res.send("hello");
-    //res.send(<landingPage>)
+
+    async function quickstart() {
+        try {
+        // Imports the Google Cloud client library
+        const vision = require('@google-cloud/vision');
+      
+        // Creates a client
+        const client = new vision.ImageAnnotatorClient();
+      
+        // Performs label detection on the image file
+        const [result] = await client.labelDetection(`./1.webp`);
+        const labels = result.labelAnnotations;
+        const data=[];
+        console.log('Labels:');
+        labels.forEach(label => data.push(label.description));
+        console.log(data);
+        
+        res.json(data);    
+        } catch (err) {
+
+            res.send({error:err.message});
+        }
+        
+
+
+      }
+      quickstart();
 })
 
 app.get('/login', (req, res) => {
@@ -27,7 +50,7 @@ app.get('/login', (req, res) => {
 
 
 console.log("welcome to Project Hub");
-const PORT = process.env.PORT || 91;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
